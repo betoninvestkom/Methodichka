449abaed7faa03d2d21b4c637a32457eccd17c19
User <User@AKB-FM-NB-X5B.nis.edu.kz>
2018-02-12 23:47:38 +0500

    Calculate area of a circle

---
+++ Methodichka/src/standard/Area_of_Circle.java
@@ -0,0 +1,15 @@
+package standard;
+import java.util.*;
+public class Area_of_Circle {
+
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter radius:");
+		float radius = input.nextFloat();
+		
+		float Area = (float) Math.PI*(radius*radius);	
+		System.out.println(Area);
+	}
+
+}
